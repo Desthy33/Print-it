2c0dad9f23bf86db3db5e5204d4d8ceb2f58bfa8
Desthy <desthywork@macbook-air-de-desthy.home>
2023-09-30 20:47:07 +0200

    etape 5 défilement

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,42 @@
+// script.js
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ];
-
 
 const arrowright = document.querySelector(".arrow_right");
 const arrowleft = document.querySelector(".arrow_left");
 
 arrowright.addEventListener("click", function () {
-    currentindex = (currentindex + 1) ;
-    navcarrousel(currentindex, "right");
-	console.log ("test arrow")},
-),
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, "right");
+    updateDots(currentIndex);
+});
 arrowleft.addEventListener("click", function () {
-    currentindex = (currentindex - 1);
-    navcarrousel(currentindex, "left");
-	console.log ("test arrow")}
-);
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, "left");
+    updateDots(currentIndex);
+});
 
-let currentindex = 0;
-const bannerimg = document.querySelector(".banner-img");
+let currentIndex = 0;
+const bannerImg = document.querySelector(".banner-img");
 const dots = document.querySelectorAll(".dot");
 
-function navcarrousel(index, direction) {
-	const imagePath = "assets/images/slideshow/${slides[currentindex].image}";
-    bannerimg.src = imagePath;
-    bannerimg.alt = "slide ${currentindex + 1}";
-	const tagLine = slides[currentindex].tagLine;
-	document.querySelector("p").innerHTML = tagLine;
-}
-
-function navdots(index) {
+function updateDots(index) {
     dots.forEach((dot, i) => {
         if (i === index) {
             dot.classList.add("dot_selected");
@@ -54,4 +46,20 @@ function navdots(index) {
     });
 }
 
+function updateCarousel(index, direction) {
+      if (currentIndex === -1 && direction === "left") {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === "right") {
+        currentIndex = 0;
+    }
 
+    const imagePath = `./assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.sc = imagePath;
+    bannerImg.alt = `slide ${currentIndex + 1}`;
+
+
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector("p").innerHTML = tagLine;
+
+    console.log(`clic ${direction}`);
+}
